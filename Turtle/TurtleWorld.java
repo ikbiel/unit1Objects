ad145c08c7f505dd3dfb11edaaac67b0dfc5efc2
ikbiel <ikbiel13@gmail.com>
2014-09-10 15:16:46 -0500

    Turtle lab to turn in

--- Turtle/TurtleWorld.java
+++ Turtle/TurtleWorld.java
@@ -2,12 +2,54 @@ import java.awt.Color;
 
 public class TurtleWorld
 {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
         World turtleWorld = new World();
-        Turtle turtle = new Turtle(turtleWorld);
-        turtle.setPenColor(Color.RED);
-        turtle.penDown();
-        turtle.forward(50);
+        
+        Turtle turtle1 = new Turtle(turtleWorld);
+        turtle1.moveTo(30, 10);
+        turtle1.setPenColor(Color.GREEN);
+        turtle1.turn(180);
+        turtle1.penDown();
+        turtle1.forward(100);
+        turtle1.penUp();
+        turtle1.turn(180);
+        
+        turtle1.moveTo(40, 50);
+        turtle1.penDown();
+        turtle1.setPenColor(Color.MAGENTA);
+        turtle1.turn(90);
+        turtle1.forward(50);
+        turtle1.turn(135);
+        turtle1.forward(60);
+        turtle1.turn(-135);
+        turtle1.forward(70);
+        turtle1.penUp();
+        turtle1.turn(-90);
+        
+        turtle1.moveTo(110, 50);
+        turtle1.penDown();
+        turtle1.setPenColor(Color.BLUE);
+        turtle1.turnRight();
+        turtle1.forward(50);
+        turtle1.turn(135);
+        turtle1.forward(60);
+        turtle1.turn(-135);
+        turtle1.forward(70);
+        turtle1.penUp();
+        
+        turtle1.moveTo(200, 10);
+        turtle1.penDown();
+        turtle1.setPenColor(Color.GREEN);
+        turtle1.turn(45);
+        turtle1.forward(100);
+        
+        
+        
+        
+     
+
+        
+       
     }
 }
